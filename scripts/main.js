c80f862f8558a413e145107eec22c95986d6ee7c
Kiisi Felix <kiisifelix06@gmail.com>
2023-02-10 13:37:51 +0100

    90% done with dark theme

--- scripts/main.js
+++ scripts/main.js
@@ -17,56 +17,74 @@ bannerCancelBtn.addEventListener("click",()=>{
 })
 
 
-/* Footer Accordion */
 
-const footerTitle = document.querySelectorAll(".footer__links--title")
-footerTitle.forEach(title =>{
+// System Theme
 
-    let footerLinks = title.nextElementSibling
-    const pointer = title.children.item(0)
-    
-    title.onclick = () =>{
-        if(!footerLinks.classList.contains("effect")){
-            pointer.textContent = "remove"
-            footerLinks.classList.add("effect")
-            footerLinks.style.height = "auto"
+const OS_darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-            let footerLinksHeight = footerLinks.clientHeight + 'px'
-            footerLinks.style.height = '0px'
-            setTimeout(()=>{
-                footerLinks.style.height = footerLinksHeight
-            },0)
-
-            
-        }else{
-            pointer.textContent = "add"
-            footerLinks.style.height = '0px'
-            footerLinks.addEventListener("transitionend",()=>{
-                footerLinks.classList.remove("effect")
-            },{once: true})
-
-        }
-    }
-})
+if(OS_darkTheme.matches){
+    darkMode()
+}
 
 /* Theme Data */
 
-let themeBtnBox = document.querySelector(".mobile-navigation__settings--item__theme")
-let themeBtn = document.querySelector(".mobile-navigation__settings--item__theme-btn div")
+let mobileThemeBtnBox = document.querySelector(".mobile-navigation__settings--item__theme")
+let mobileThemeBtn = document.querySelector(".mobile-navigation__settings--item__theme-btn div")
+
+let mobileThemeBtnSvgLightMode = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="css-41qi6t" width="24"  height="24" ><defs><symbol viewBox="0 0 24 24" id="mode-light"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 2H13.5V5H10.5V2ZM16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12ZM5.98948 3.86891L3.86816 5.99023L5.98948 8.11155L8.1108 5.99023L5.98948 3.86891ZM2 13.5V10.5H5V13.5H2ZM3.86794 18.0095L5.98926 20.1309L8.11058 18.0095L5.98926 15.8882L3.86794 18.0095ZM13.5 19V22H10.5V19H13.5ZM18.01 15.8884L15.8887 18.0098L18.01 20.1311L20.1313 18.0098L18.01 15.8884ZM19 10.5H22V13.5H19V10.5ZM15.8894 5.99001L18.0107 8.11133L20.1321 5.99001L18.0107 3.86869L15.8894 5.99001Z" fill="currentColor"></path></symbol></defs><use xlink:href="#mode-light" fill="#707A8A"></use></svg>`
+
+let mobileThemeBtnSvgDarkMode = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="css-1biqlx6" width="24"  height="24" ><defs><symbol viewBox="0 0 24 24" id="mode-dark"><path d="M20.9677 12.7676C19.84 13.5449 18.4732 13.9999 17 13.9999C13.134 13.9999 10 10.8659 10 6.99994C10 5.52678 10.4551 4.15991 11.2323 3.03223C6.62108 3.42175 3 7.28797 3 11.9999C3 16.9705 7.02944 20.9999 12 20.9999C16.712 20.9999 20.5782 17.3789 20.9677 12.7676Z" fill="currentColor"></path></symbol></defs><use xlink:href="#mode-dark" fill="#707A8A"></use></svg>`
 
 let themeBtnCurrentState = false
-themeBtnBox.addEventListener("click", ()=>{
+mobileThemeBtnBox.addEventListener("click", ()=>{
     if(themeBtnCurrentState){
-        themeBtn.classList.remove("active")
+        mobileThemeBtnBox.setAttribute('aria-mobile-theme-btn', 'false')
         themeBtnCurrentState = false;
-        themeBtn.innerHTML = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="css-41qi6t" width="24"  height="24" ><defs><symbol viewBox="0 0 24 24" id="mode-light"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 2H13.5V5H10.5V2ZM16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12ZM5.98948 3.86891L3.86816 5.99023L5.98948 8.11155L8.1108 5.99023L5.98948 3.86891ZM2 13.5V10.5H5V13.5H2ZM3.86794 18.0095L5.98926 20.1309L8.11058 18.0095L5.98926 15.8882L3.86794 18.0095ZM13.5 19V22H10.5V19H13.5ZM18.01 15.8884L15.8887 18.0098L18.01 20.1311L20.1313 18.0098L18.01 15.8884ZM19 10.5H22V13.5H19V10.5ZM15.8894 5.99001L18.0107 8.11133L20.1321 5.99001L18.0107 3.86869L15.8894 5.99001Z" fill="currentColor"></path></symbol></defs><use xlink:href="#mode-light" fill="#707A8A"></use></svg>`
+        mobileThemeBtn.innerHTML = mobileThemeBtnSvgLightMode
+        lightMode()
         
     }else{
-        themeBtn.classList.add("active")
+        mobileThemeBtnBox.setAttribute('aria-mobile-theme-btn', 'true')
         themeBtnCurrentState = true;
-        themeBtn.innerHTML = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="css-1biqlx6" width="24"  height="24" ><defs><symbol viewBox="0 0 24 24" id="mode-dark"><path d="M20.9677 12.7676C19.84 13.5449 18.4732 13.9999 17 13.9999C13.134 13.9999 10 10.8659 10 6.99994C10 5.52678 10.4551 4.15991 11.2323 3.03223C6.62108 3.42175 3 7.28797 3 11.9999C3 16.9705 7.02944 20.9999 12 20.9999C16.712 20.9999 20.5782 17.3789 20.9677 12.7676Z" fill="currentColor"></path></symbol></defs><use xlink:href="#mode-dark" fill="#707A8A"></use></svg>`
+        mobileThemeBtn.innerHTML = mobileThemeBtnSvgDarkMode
+        darkMode()
     }
 })
+
+
+/* Dark-Light Theme */
+
+let desktopThemeBtn = document.querySelector(".navigation__sub--settings__theme")
+
+desktopThemeBtn.addEventListener("click", ()=>{
+    if(desktopThemeBtn.textContent === "dark_mode"){
+        darkMode()
+    }else{
+        lightMode()
+    }
+})
+
+function darkMode(){
+    document.querySelector("body").classList.add("dark-mode--theme")
+    document.querySelector(".header__main--image img").src = './assets/images/foreground-image-dark.png';
+    desktopThemeBtn.textContent = "light_mode"
+    mobileThemeBtnBox.setAttribute('aria-mobile-theme-btn', 'true')
+    mobileThemeBtn.innerHTML = mobileThemeBtnSvgDarkMode
+}
+
+function lightMode(){
+    document.querySelector("body").classList.remove("dark-mode--theme")
+    document.querySelector(".header__main--image img").src = './assets/images/foreground-image-light.png'
+    desktopThemeBtn.textContent = "dark_mode"
+    mobileThemeBtnBox.setAttribute('aria-mobile-theme-btn', 'false')
+    mobileThemeBtn.innerHTML = mobileThemeBtnSvgLightMode
+}
+
+
+// Rendering Copy write year
+
+let date = new Date()
+document.querySelector(".copyright__text span").textContent = date.getFullYear()
 
 
 /* Menu bar and Mobile Menu Toggle Display */
@@ -130,34 +148,34 @@ mobileNavigationTitle.forEach(title =>{
 })
 
 
-/* Dark-Light Theme */
+/* Footer Accordion */
 
-let themeColorBtn = document.querySelector(".navigation__sub--settings__theme")
+const footerTitle = document.querySelectorAll(".footer__links--title")
+footerTitle.forEach(title =>{
 
-themeColorBtn.addEventListener("click", ()=>{
-    if(themeColorBtn.textContent === "dark_mode"){
-        themeColorBtn.textContent = "light_mode"
-        darkMode()
-    }else{
-        themeColorBtn.textContent = "dark_mode"
-        lightMode()
+    let footerLinks = title.nextElementSibling
+    const pointer = title.children.item(0)
+    
+    title.onclick = () =>{
+        if(!footerLinks.classList.contains("effect")){
+            pointer.textContent = "remove"
+            footerLinks.classList.add("effect")
+            footerLinks.style.height = "auto"
+
+            let footerLinksHeight = footerLinks.clientHeight + 'px'
+            footerLinks.style.height = '0px'
+            setTimeout(()=>{
+                footerLinks.style.height = footerLinksHeight
+            },0)
+
+            
+        }else{
+            pointer.textContent = "add"
+            footerLinks.style.height = '0px'
+            footerLinks.addEventListener("transitionend",()=>{
+                footerLinks.classList.remove("effect")
+            },{once: true})
+
+        }
     }
 })
-
-function darkMode(){
-    document.querySelector("body").classList.add("dark-mode--theme")
-    document.querySelector(".header__main--image img").src = './assets/images/foreground-image-dark.png'
-    
-}
-
-function lightMode(){
-    document.querySelector("body").classList.remove("dark-mode--theme")
-    document.querySelector(".header__main--image img").src = './assets/images/foreground-image-light.png'
-    
-}
-
-
-// Rendering Copy write year
-
-let date = new Date()
-document.querySelector(".copyright__text span").textContent = date.getFullYear()
